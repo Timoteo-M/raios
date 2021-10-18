73e7f9e37ad060c1e5f02bf0a3c50c149a2c2014
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-10-18 18:38:16 -0300

    mais estilo

--- scripts.js
+++ scripts.js
@@ -8,3 +8,6 @@ function myFunction() {
     var x4 = document.getElementById("myRange").value;
     document.getElementById("demo").innerHTML = x4;
 }
+function copiar(){
+    alert("Esse botão ainda não funciona, volte mais tarde!!");
+}
