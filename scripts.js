df086722a5163131cffa2c7e5a987a198f263633
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-10-18 14:58:55 -0300

    up

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x1 = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x1;
+    var x2 = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x2;
+    var x3 = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x3;
+    var x4 = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x4;
+}
